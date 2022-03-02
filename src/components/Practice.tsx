d8e3a9eabac7c5378f9153e42d3baa73843145ff
ayan-afk <ayanparvez7864@gmail.com>
2022-03-02 19:27:48 +0530

    just for ....

---
+++ src/components/Practice.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../index.css';
+
+const Practice: React.FC = () => {
+  return (
+    <div className="bg-blue-700 container">
+      <div className="bg-blue-700">
+        <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded">Hello</button>
+      </div>
+    </div>
+  )
+}
+
+
+export default Practice
